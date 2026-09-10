185305b3c3fa1adb27129a8f4eec0b26e213872c
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-09-10 11:27:24 -0700

    Update server header and add IP extraction and whoami endpoint

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
+	"net"
 	"net/http"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
-// ServerHeader adds the server name to each response.
 func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c *echo.Context) error {
-		c.Response().Header().Set(echo.HeaderServer, "Old-Echo-Server")
+		c.Response().Header().Set(echo.HeaderServer, "New-Echo-Server")
 		return next(c)
 	}
 }
 
 func main() {
 	e := echo.New()
+	e.IPExtractor = echo.ExtractIPDirect()
+
 	e.Use(middleware.Recover())
 	e.Use(middleware.RequestID())
 	e.Use(ServerHeader)
 
 	e.GET("/", func(c *echo.Context) error {
 		return c.String(http.StatusOK, "OK")
+	})
+
+	e.GET("/whoami", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]any{"ip": c.RealIP()})
 	})
 
 	if err := e.Start(":1323"); err != nil {
